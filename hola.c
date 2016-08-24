16e3217a8c5bbf584726ed75b71f905cc68730ba
Diamefbaal <dmbm9999@gmail.com>
2016-08-24 11:53:02 -0500

    primer trabajo

---
+++ hola.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+int main() {
+
+	int num1;
+	int num2;
+	int suma;
+
+
+	printf("Bienvenido");
+
+	printf("Inserte un valor:\n");
+	scanf("%d", &num1);
+
+	print("Inserte otro valor:\n");
+	scanf("%d", &num2);
+
+	suma = num1 + num2;
+
+
+
+
+
+	printf("La suma es %d\b", suma);
+	return 0;
+}
